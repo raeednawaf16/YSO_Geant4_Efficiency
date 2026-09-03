45356a4cadad0896597f5a0c037532736e701166
raeednawaf16 <raeednawaf16@gmail.com>
2026-09-03 11:10:47 -0400

    added build folder

---
+++ build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/sim.cc" "CMakeFiles/sim.dir/sim.cc.o" "gcc" "CMakeFiles/sim.dir/sim.cc.o.d"
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/src/ActionInitialization.cc" "CMakeFiles/sim.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/sim.dir/src/ActionInitialization.cc.o.d"
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/src/Clover.cc" "CMakeFiles/sim.dir/src/Clover.cc.o" "gcc" "CMakeFiles/sim.dir/src/Clover.cc.o.d"
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/src/Constants.cc" "CMakeFiles/sim.dir/src/Constants.cc.o" "gcc" "CMakeFiles/sim.dir/src/Constants.cc.o.d"
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/src/DetectorConstruction.cc" "CMakeFiles/sim.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/sim.dir/src/DetectorConstruction.cc.o.d"
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/src/EventAction.cc" "CMakeFiles/sim.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/EventAction.cc.o.d"
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/src/PhysicsList.cc" "CMakeFiles/sim.dir/src/PhysicsList.cc.o" "gcc" "CMakeFiles/sim.dir/src/PhysicsList.cc.o.d"
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/src/PrimaryGeneratorAction.cc" "CMakeFiles/sim.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/src/RunAction.cc" "CMakeFiles/sim.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/RunAction.cc.o.d"
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/src/SteppingAction.cc" "CMakeFiles/sim.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/SteppingAction.cc.o.d"
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/src/TrackerGammaHit.cc" "CMakeFiles/sim.dir/src/TrackerGammaHit.cc.o" "gcc" "CMakeFiles/sim.dir/src/TrackerGammaHit.cc.o.d"
+  "/home/raeedlinux/Raeed/YSO_Geant4_efficiency/src/TrackerGammaSD.cc" "CMakeFiles/sim.dir/src/TrackerGammaSD.cc.o" "gcc" "CMakeFiles/sim.dir/src/TrackerGammaSD.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
